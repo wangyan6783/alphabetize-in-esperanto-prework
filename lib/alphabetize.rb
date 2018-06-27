69e14211a9f51e5f02639cf0ac374d8159c21ff4
Yan Wang <wangyan6783@gmail.com>
2018-06-27 12:22:12 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,9 +1,9 @@
 def alphabetize(arr)
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  arr.sort_by! {|ele|
+  sorted = arr.sort_by {|ele|
     ele.each_char { |char|
       alphabet.index(char)
     }
   }
-  arr
+  sorted
 end
