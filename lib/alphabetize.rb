75678b789871c3d2d07b17f5728f8a2263b76e4c
Yan Wang <wangyan6783@gmail.com>
2018-06-27 12:43:44 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,38 +1,13 @@
 ESPERANTO_ALPHABET = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
-def alphabetize(arr)
-  sorted = arr.sort_by {|ele|
-    ele.each_char { |char|
-      ESPERANTO_ALPHABET.index(char)
-    }
-  }
-end
-
 # def alphabetize(arr)
-#   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 #   sorted = arr.sort_by {|ele|
-#     ele.split("").each { |char|
-#       alphabet.index(char)
+#     ele.each_char { |char|
+#       ESPERANTO_ALPHABET.index(char)
 #     }
 #   }
-#   # alphabet.index("ĉ")
-#   sorted
 # end
 
-
-# def alphabetize(arr)
-#   # code here
-#   list  = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split("")
-  
-#   new_arr = arr.sort_by{|sentence|
-#     sentence.split(" ").sort_by{|word|
-#       word.split("").sort_by{|char|
-#         list.index(char)
-#       }
-#     } 
-#   }
-  
-#   new_arr.sort_by{|sentence|
-#     list.index(sentence[0])
-#   }
-# end
+def alphabetize(arr)
+  arr.sort_by { |a| a.split("").map{ |char| ESPERANTO_ALPHABET.index(char) } }
+end
