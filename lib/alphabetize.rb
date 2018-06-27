e0302dc68b3545206f312c49150096d62dd16418
Yan Wang <wangyan6783@gmail.com>
2018-06-27 12:37:20 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,9 +1,20 @@
+# def alphabetize(arr)
+#   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+#   sorted = arr.sort_by {|ele|
+#     ele.each_char { |char|
+#       alphabet.index(char)
+#     }
+#   }
+#   sorted
+# end
+
 def alphabetize(arr)
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   sorted = arr.sort_by {|ele|
-    ele.each_char { |char|
+    ele.split("").each { |char|
       alphabet.index(char)
     }
   }
+  # alphabet.index("ĉ")
   sorted
 end
