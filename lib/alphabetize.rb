2290534a8e15cd4c0b1a3d651aa50e0d9f61c8e0
Yan Wang <wangyan6783@gmail.com>
2018-06-27 12:45:57 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,7 @@
 ESPERANTO_ALPHABET = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
-  sorted = arr.sort_by {|ele|
+  arr.sort_by {|ele|
     ele.each_char { |char|
       ESPERANTO_ALPHABET.index(char)
     }
@@ -10,4 +10,12 @@ end
 
 def alphabetize(arr)
   arr.sort_by { |a| a.split("").map{ |char| ESPERANTO_ALPHABET.index(char) } }
+end
+
+def alphabetize(arr)
+  arr.sort_by {|ele|
+    ele.split("").each { |char|
+      ESPERANTO_ALPHABET.index(char)
+    }
+  }
 end
